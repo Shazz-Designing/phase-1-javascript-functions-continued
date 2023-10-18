001663815d80410b93fa2c156646a722e6a17102
sharon <sapot@safaricom.co.ke>
2023-10-18 07:26:39 +0300

    Add wrap adjective

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function mondayWork(activity = "go to the office") {
     return `This Monday, I will ${activity}.`;
   }
+
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
   
